68ac3ad749f3dddc7da9e7e2fb708985124ce30e
Jophy Johnson <JJohnson@MacUsers-MacBook-Pro.local>
2016-03-25 11:54:32 -0500

    removed comments

--- sound_of_code.js
+++ sound_of_code.js
@@ -1,19 +1,20 @@
 if (Meteor.isClient) {
   index = 0;
-  var arrayHolder = ['cout', 'o', 'u', 'cout', 's', 'e', 'r', 'cout', 'o', 'u', 't'];
-  var container;
-  var newLineHolder;
-  var spaceHolder;
   var counter = 0;
   var holder;
-  var extraHolder;
   var holderArray = [];
   var number = 0;
 
   soundManager.onready(function() {
     if (soundManager.supported()) {
       // SM2 is ready to go!
-      var mySound = soundManager.createSound({id: 'mySound', url: 'http://www.vibrationdata.com/piano_E.mp3', volume: 50, multiShot: true, duration: 1000});
+      var mySound = soundManager.createSound({
+        id: 'mySound',
+        url: 'http://www.vibrationdata.com/piano_E.mp3',
+        volume: 50,
+        multiShot: true,
+        duration: 1000
+      });
     } else {
       console.log("something went wrong")
     }
@@ -39,7 +40,6 @@ if (Meteor.isClient) {
   function playSound() {
     soundManager.play('mySound', {
       onfinish: function() {
-        //arrayTest(holderArray, holderArray.length);
         arrayTest(holderArray, holderArray.length);
       }
     });
@@ -58,22 +58,19 @@ if (Meteor.isClient) {
           if (holder[x + 1] == 'o') {
             if (holder[x + 2] == 'u') {
               if (holder[x + 3] == 't') {
-                //console.log("found it!!");
                 holderArray[number] = 'cout';
                 number++;
               }
             }
           }
         } else {
-          //console.log("sigh");
           continue;
         }
       }
-      for(var y = 0; y < holderArray.length; y++){
+      for (var y = 0; y < holderArray.length; y++) {
         console.log(holderArray[y]);
       }
       arrayTest(holderArray, holderArray.length);
-      //arrayTest(arrayHolder, arrayHolder.length);
     }
 
   });
